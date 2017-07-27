0edae08a8a0c02667aa8bfe1a87f6ae52a6ed318
eacevedof <eacevedof@yahoo.es>
2017-07-27 12:08:14 +0200

    file component_scandir.php version 1.0.0  date 27-07-2017 12:06

--- vendor/theframework/components/component_scandir.php
+++ vendor/theframework/components/component_scandir.php
@@ -2,22 +2,73 @@
 /**
  * @author Eduardo Acevedo Farje.
  * @link www.eduardoaf.com
- * @version 1.0.0
  * @name ComponentScandir
- * @date 01-06-2014 12:45
  * @file component_scandir.php
+ * @version 1.0.0 
+ * @date 27-07-2017 12:06
  * @observations
  */
 namespace TheFramework\Components;
 
 class ComponentScandir 
 {
-    
-    
+    private $arPaths;
+    private $arFiles;
+
     public function __construct() 
     {
+        $this->arPaths = [
+            "C:/xampp/htdocs/proy_hydra_flamagas/dts/update_20170620_pricing",
+            "C:/xampp/htdocs/proy_hydra_flamagas/dts/update_20170705_tablas_enblanco",
+            "C:/xampp/htdocs/proy_hydra_flamagas/dts/update_20170706_campo_en_knb1"
+        ];
         
+        $this->arFiles = [];
     }
-        
+    
+    private function in_string($arChars=[],$sString)
+    {
+        foreach($arChars as $c)
+            if(strstr($sString,$c))
+                return TRUE;
+        return FALSE;
+    }
+    
+    private function clean($arSubstrings=[],&$sString)
+    {
+        $sReplace = $sString;
+        foreach($arSubstrings as $str)
+            $sReplace = str_replace ($str,"",$sReplace);
+        $sString = $sReplace;
+    }
+    
+    private function get_files()
+    {
+        foreach($this->arPaths as $sPath)
+        {
+            $arFiles = scandir($sPath);
+            $sPath = explode("/",$sPath);
+            $sPath = end($sPath);
+            foreach($arFiles as $sFileName)
+            {
+                if($this->in_string([".XNT"],$sFileName))
+                {
+                    $this->clean([".XNT"],$sFileName);
+                    $sFileName = substr($sFileName,14);
+                    $this->arFiles[$sPath][] = $sFileName;
+                }
+            }
+            asort($this->arFiles[$sPath]);
+        }
+        return $this->arFiles;
+    }
+    
+    public function run()
+    {
+        $arFiles = $this->get_files();
+        pr($arFiles);
+    }
+
+    public function add_path($value){}
     
 }//ComponentScandir
