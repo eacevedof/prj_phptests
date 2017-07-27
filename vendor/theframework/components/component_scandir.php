4509cd5966ad1c9b5e5619d3f96cbd0fa9788a65
eacevedof <eacevedof@yahoo.es>
2017-07-27 11:06:21 +0200

    backup ComponentScandir

---
+++ vendor/theframework/components/component_scandir.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @version 1.0.0
+ * @name ComponentScandir
+ * @date 01-06-2014 12:45
+ * @file component_scandir.php
+ * @observations
+ */
+namespace TheFramework\Components;
+
+class ComponentScandir 
+{
+    
+    
+    public function __construct() 
+    {
+        
+    }
+        
+    
+}//ComponentScandir
