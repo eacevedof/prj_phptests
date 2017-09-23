e36595467f956547dc06b5b438fc3ef4be9716f9
eacevedof <eacevedof@yahoo.es>
2017-09-23 12:19:10 +0200

    definicion

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,21 @@
+//http://json.theframework.es/data/app_costumer.json
 console.log("promises")
+(()=>{
+    function getUsers(){
+        setTimeout(function(){
+            console.log("Users are Ready")
+        },800)
+    }
+    
+    function getCustomers(){
+        setTimeout(function(){
+            console.log("Customers are Ready")
+        },400)
+    }    
+    
+    function getProducts(){
+        setTimeout(function(){
+            console.log("Products are Ready")
+        },800)
+    }    
+})
