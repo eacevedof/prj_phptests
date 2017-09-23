10332f1634c46d6983bbc15759911f4d54e273f7
eacevedof <eacevedof@yahoo.es>
2017-09-23 17:23:24 +0200

    getDataByAjax ahora devuelve una promesa

--- js/jquery-deferred.js
+++ js/jquery-deferred.js
@@ -13,26 +13,14 @@
         //con lo cual cuenta con un metodo .then
         //y etados pending, rejected y fullfill
         //y tiene otro metodo promise() que hace la transformacion
-        let oJqAjax = $.ajax({
+        let oPromise = $.ajax({
             url: sServerUrl.concat(sFile),
-            headers:{
-                //estas cabeceras se usan para APIS que acepta CORS
-                //"Content-Type":"application/json",
-                //"Api-Token":"1234"
-            },
-            success:(sResponse)=>{
-                console.info("users",sResponse)
-            },
-            error:(sResponse)=>{
-                console.log("error",sResponse)
-            }
         }).promise()//promise: transforma el deferred object a una promesa
         
-        //devuelve un objeto deferido
-        return oJqAjax;
+        return oPromise;
     }//getDataByAJAX
     
-    let oJqAjax = getDataByAJAX("base_user.json")
+    let oPromise = getDataByAJAX("base_user.json")
     //console.log("oJqAjax",oJqAjax)
 
 })(jQuery);
