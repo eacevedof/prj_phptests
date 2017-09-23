45408e251f6374d63932cc11f3a97d8ceed5b2f2
eacevedof <eacevedof@yahoo.es>
2017-09-23 17:17:40 +0200

    getDataByAjax, oJqAjax.promise(): transforma el deferred object a una promesa

--- js/jquery-deferred.js
+++ js/jquery-deferred.js
@@ -1,21 +1,33 @@
 //webtraining.zone
 ////file: jquery-deferred.js (diferido)
 //definimos un scope
+
+//app_product_family.csv
 (($)=>{
     //Deferred Objects
-    let sServerUrl = "http://json.theframework.es/index.php?getfile=";
-    $.ajax({
-        url: sServerUrl.concat("base_user.json"),
-        headers:{
-            //estas cabeceras se usan para APIS que acepta CORS
-            //"Content-Type":"application/json",
-            //"Api-Token":"1234"
-        },
-        success:(sResponse)=>{
-            console.info("users",sResponse)
-        },
-        error:(sResponse)=>{
-            console.log("error",sResponse)
-        }
-    })
+    let sServerUrl = "http://json.theframework.es/index.php?getfile="
+    
+    let getDataByAJAX = (sFile)=>{
+        let oJqAjax = $.ajax({
+            url: sServerUrl.concat(sFile),
+            headers:{
+                //estas cabeceras se usan para APIS que acepta CORS
+                //"Content-Type":"application/json",
+                //"Api-Token":"1234"
+            },
+            success:(sResponse)=>{
+                console.info("users",sResponse)
+            },
+            error:(sResponse)=>{
+                console.log("error",sResponse)
+            }
+        }).promise()
+        
+        //devuelve un objeto deferido
+        return oJqAjax;
+    }//getDataByAJAX
+    
+    let oJqAjax = getDataByAJAX("base_user.json")
+    //console.log("oJqAjax",oJqAjax)
+
 })(jQuery);
