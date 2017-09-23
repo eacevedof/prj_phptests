f5d324c0d0106f6b9a7026c4387485803c25ec5c
eacevedof <eacevedof@yahoo.es>
2017-09-23 12:35:12 +0200

    ya funciona hay que llamar a fnResolve en la funcion anonima

--- js/promises.js
+++ js/promises.js
@@ -2,27 +2,30 @@
 console.log("file:promises.js");
 (function(){
     function getUsers(){
-        let oPromise = new Promise(function(oResolve,oReject){
+        let oPromise = new Promise(function(fnResolve,oReject){
             setTimeout(function(){
                 console.log("Users are Ready")
+                fnResolve()
             },800)            
         })
         return oPromise
     }
     
     function getCustomers(){
-        let oPromise = new Promise(function(oResolve,oReject){
+        let oPromise = new Promise(function(fnResolve,oReject){
             setTimeout(function(){
                 console.log("Customers are Ready")
+                fnResolve()
             },400)
         })
         return oPromise
     }    
     
     function getProducts(){
-        let oPromise = new Promise(function(oResolve,oReject){
+        let oPromise = new Promise(function(fnResolve,oReject){
             setTimeout(function(){
                 console.log("Products are Ready")
+                fnResolve()
             },600)
         })
         return oPromise
