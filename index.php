81cfdd7bdd1161a06bfc95635ca289a6aaa00bec
eacevedof <eacevedof@yahoo.es>
2017-10-21 12:12:39 +0200

    pruebas helper data

--- index.php
+++ index.php
@@ -12,14 +12,31 @@ require_once "vendor/autoload.php";
 require_once "vendor/theframework/components/autoload.php";
 
 //extrae los archvios tratadas en la dts
-use TheFramework\Components\ComponentExtract;
-$oComp = new ComponentExtract();
+//use TheFramework\Components\ComponentExtract;
+//$oComp = new ComponentExtract();
 //$oComp->run(0);
 
 //archivos .XNT que nos han proporcionado
-$oComp = new \TheFramework\Components\ComponentScandir();
+//$oComp = new \TheFramework\Components\ComponentScandir();
 //$oComp->run();
 
 //recupero los alter table
-$oComp = new \TheFramework\Components\ComponentHydrapk();
-$oComp->run();
+//$oComp = new \TheFramework\Components\ComponentHydrapk();
+//$oComp->run();
+
+/*
+<div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+ *  */
+use TheFramework\Helpers\HelperLabel;
+use TheFramework\Helpers\HelperDate;
+
+$oLabel = new HelperLabel("datBirthdate","Birthdate","lblBirthdate");
+$oDate = new HelperDate();
+$oDate->set_type("date");
+$oDate->set_id("datBirthdate");
+$oDate->set_name("datBirthdate");
+$oDate->show();
