e87e77a9faaba6476918bffe9a741f7f274345c7
Eduardo A.F <eacevedof@yahoo.es>
2018-02-25 15:33:08 +0100

    index.php 2.0.0 Zonas comentadas para ejemplos de helpers y componentes

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-//index.php 1.0.0 Extract mejorado
+//index.php 2.0.0 Extract mejorado
 //carga el loader de composer. Este loader solo tiene registrado el loader de helpers.
-require_once "vendor/autoload.php";
+/**
+ * HELPERS
+ **/
+require_once "vendor/autoload.php";//generdo por composer
 use TheFramework\Helpers\HelperDiv;
 $oDiv = new HelperDiv();
 $oDiv->set_innerhtml("some div");
@@ -10,9 +13,11 @@ $oDiv->show();
 $oRaw = new TheFramework\Helpers\HelperRaw("<p>hello</p>");
 $oRaw->show();
 
+/**
+ * COMPONENTES
+ **/
 
-//require_once "vendor/theframework/components/autoload.php";
-
+require_once "vendor/theframework/components/autoload.php";
 //extrae los archvios tratadas en la dts
 use TheFramework\Components\ComponentExtract;
 $oComp = new ComponentExtract();
