4fd94cce32470524bedf0600df46b062a29edfcd
Eduardo A.F <eacevedof@yahoo.es>
2018-03-03 17:23:07 +0100

    run.php 2.0.0

--- vendor/theframework/components/console/run.php
+++ vendor/theframework/components/console/run.php
@@ -1,18 +1,21 @@
 <?php
-//run.php 1.0.0
+//run.php 2.0.0
 include("component_console.php");
 
-
-
-$sAction = "process_files";
+/*
+php run.php $argv[1]                $argv[2]                    $argv[3]    $argv[4..n]
+php run.php <path_file_to_include>  <class_name_case_sensitive> <method>    <rest-of-arguments>
+*/
 if(defined("STDIN"))
 {
-    //if(!empty($argv[1])) parse_str($argv[1],$_GET);
-    if(isset($argv[1]))
-        $sAction = $argv[1];
+    $oRunner = new TheFramework\Components\Console\ComponentConsole($argv);
+    $oRunner->set_pathclass($argv[1]);
+    $oRunner->set_classname($argv[2]);
+    $oRunner->set_method($argv[3]);
+    //$oRunner->set_arguments($argv);
+}
+else
+{
+    echo "Not console called";
 }
 
-$oMain = new TheFramework\Components\Console();
-$oMain->set_action($sAction);
-//$oMain->run(1); //prod
-$oMain->run(0); //local
