eeded4a907d2cd0775cd99d7d22eaa7294498c64
eacevedof <eacevedof@yahoo.es>
2018-03-19 17:51:57 +0100

    ComponentAsinserts  pendiente de hacer

---
+++ vendor/theframework/components/component_asinserts.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name ComponentAsinserts
+ * @file component_asinserts.php
+ * @version 1.0.0
+ * @date 19-03-2018 16:31
+ * @observations
+ */
+namespace TheFramework\Components;
+
+use TheFramework\Components\ComponentSqlserver;
+
+class ComponentAsinserts 
+{
+    public function __construct() 
+    {
+        
+    }
+
+    public function get_tables()
+    {
+        
+    }
+    
+    public function get_fields($sTable)
+    {
+        
+    }
+    
+    
+    public function debug($mxVar){echo var_export($mxVar,1)."\n";}  
+}//ComponentAsinserts
