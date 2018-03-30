824b754a51432e10f38bbc35f92a61a791254b39
eacevedof <eacevedof@yahoo.es>
2018-03-30 13:54:43 +0200

    autoload.php 2.0.0

--- vendor/theframework/components/autoload.php
+++ vendor/theframework/components/autoload.php
@@ -1,6 +1,6 @@
 <?php
 //components autoload
-//autoload.php 1.0.0
+//autoload.php 2.0.0
 $sPathRoot = dirname(__FILE__).DIRECTORY_SEPARATOR;
 //die("sPathRoot: $sPathRoot");//...tests\vendor\theframework\components
 $arSubFolders[] = get_include_path();
@@ -15,12 +15,13 @@ set_include_path($sPathInclude);
 
 spl_autoload_register(function($sNSClassName)
 {
-    //Array ( [0] => TheFramework [1] => Components [2] => Db [3] => ComponentMssql )
     $arClass = explode("\\",$sNSClassName);
     $sClassName = end($arClass);
+    $sClassName = preg_replace("/(?<!^)([A-Z])/","_\\1",$sClassName);
     $sClassName = str_replace("Component","",$sClassName);
     $sClassName = strtolower($sClassName);
-    $sClassName = "component_$sClassName.php";
+    //if(strstr($sClassName,"xp"))die($sClassName);
+    $sClassName = "component$sClassName.php";
     if(stream_resolve_include_path($sClassName))
         include_once $sClassName;
     elseif(function_exists("lg"))
