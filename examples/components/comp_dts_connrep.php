08f377fd7a7cc59fbf7b57b9138ed050a90e3448
eacevedof <eacevedof@yahoo.es>
2018-03-31 20:50:21 +0200

    comp_dts_connrep.php 1.0.0

---
+++ examples/components/comp_dts_connrep.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * @file: comp_dts_connrep.php 1.0.0
+ * @info: proyecto Flamagas
+ * extrae todos los nombres de los archivos configurados en las dts y recupera de 
+ * ERP_IMP_Taules todos estos de modo que se pueda comprobar los que faltan
+*/
+include("vendor/theframework/components/autoload.php");
+
+use TheFramework\Components\ComponentDtsConnrep;
+$oComp = new ComponentDtsConnrep();
+//0 no imprime por pantalla.
+$oComp->run(0);
+
