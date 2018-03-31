027726f14261244495a96fe38346839e3af51019
eacevedof <eacevedof@yahoo.es>
2018-03-31 19:45:17 +0200

    comp_dts_queryrep.php 1.0.0

---
+++ examples/components/comp_dts_queryrep.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * @file: comp_dts_queryrep.php 1.0.0
+ * @info: proyecto Flamagas
+ * Genera consulta del tipo: 
+ * DELETE FROM ERP_T001 FROM ERP_T001_AUX INNER JOIN ERP_T001 ON ERP_T001_AUX.BUKRS = ERP_T001.BUKRS
+*/
+include("vendor/theframework/components/autoload.php");
+$oComp = new TheFramework\Components\ComponentDtsQueryrep();
+$oComp->get_all();
+
+ 
+
