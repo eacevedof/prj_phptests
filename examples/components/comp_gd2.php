84cecb8115de48192f46a0aed471c8daec1cc1db
eacevedof <eacevedof@yahoo.es>
2018-03-31 17:43:19 +0200

    comp_gd2 1.0.0

--- examples/components/comp_gd2.php
+++ examples/components/comp_gd2.php
@@ -1,9 +1,12 @@
 <?php
-//file: comp_gd2.php 1.0.0
 /*
- * NOTES:
- * 
+ * @file: comp_gd2 1.0.0
+ * @info: proyecto Hydra
+ * Clase prototipo para trabajar con gd2
+ * Espera variables globales propias de hydra. No se ha terminado, se quedo en fase de prueba
+ * contiene errores.
 */
+include("vendor/theframework/components/autoload.php");
 use TheFramework\Components\ComponentGd2;
 $oGd2 = new ComponentGd2();
 $oGd2->add_from("pathfolder","C:\\Users\\eduardo alexei\\Desktop\\upload");
