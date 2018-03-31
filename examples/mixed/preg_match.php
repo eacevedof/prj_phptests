a58ed57f82fd2816aaee1019a814e1ae043ef544
eacevedof <eacevedof@yahoo.es>
2018-03-31 15:37:02 +0200

    preg_match.php 1.0.0 bakcup

--- examples/mixed/preg_match.php
+++ examples/mixed/preg_match.php
@@ -5,23 +5,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$sFile = $_SERVER["DOCUMENT_ROOT"].DIRECTORY_SEPARATOR."examples/components/component_dts_auxrep.php";
-$sContent = file_get_contents($sFile);
+$sPathFile = $_SERVER["DOCUMENT_ROOT"].DIRECTORY_SEPARATOR."examples/components/component_dts_auxrep.php";
+$sContent = file_get_contents($sPathFile);
 //extrae substring
-$arMatches = [];
-preg_match("/id=([0-9]+)\?/",$sContent, $arMatches);
+$arMatches = []; $arMatchAll = [];
+$sPattern = "/@info\:(.*?)\*\/|@[a-z]\:/s";
+$sPattern = "/@[a-z,A-Z]*\:(.*?)((@[a-z,A-Z]*\:))/s";
+
+//\*\/|
+preg_match($sPattern,$sContent,$arMatches);
+//preg_match_all($sPattern,$sContent,$arMatchAll);
 
 //https://autohotkey.com/docs/misc/RegEx-QuickRef.htm
 // (?<=...) and (?<!...) are positive and negative look-behinds (respectively) 
 // because they look to the left of the current position rather than the right 
-preg_match("/(?<!^)([A-Z])/","ComponentDtsxrepSuperBowl",$arMatches);
+//preg_match("/(?<!^)([A-Z])/","ComponentDtsxrepSuperBowl",$arMatches);
 
 //buscan por la izquierda
 //resultado:Component_Dtsxrep_Super_Bowl
-$arMatches[] = preg_replace("/(?<!^)([A-Z])/","_$1","ComponentDtsxrepSuperBowl");
+//$arMatches[] = preg_replace("/(?<!^)([A-Z])/","_$1","ComponentDtsxrepSuperBowl");
 //resultado: _omponentDtsxrepSuperBowl
-$arMatches[] = preg_replace("/(?<=^)([A-Z])/","_$1","ComponentDtsxrepSuperBowl");
+//$arMatches[] = preg_replace("/(?<=^)([A-Z])/","_$1","ComponentDtsxrepSuperBowl");
 echo "<pre>";
 print_r($arMatches);
+print_r($arMatchAll);
 echo "<hr/>";
 echo htmlentities($sContent);
