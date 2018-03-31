6e4d4a533ff01132234a6465790323d08b7f03fb
eacevedof <eacevedof@yahoo.es>
2018-03-31 17:26:49 +0200

    comp_dts_extract 1.0.0

--- examples/components/comp_dts_extract.php
+++ examples/components/comp_dts_extract.php
@@ -1,11 +1,26 @@
 <?php
-//file: comp_gd2.php 1.0.0
 /*
- * NOTES:
+ * @file: comp_dts_extract 1.0.0
+ * @info: proyecto Flamagas
+ * Imprime por pantalla un array con los nombres de los directorios y sus archivos 
+ * tipo <fecha><nombre-archivo>.XNT
+ * lee en rutas:
+ *  "C:/xampp/htdocs/proy_hydra_flamagas/dts/update_20170620_pricing",
+    "C:/xampp/htdocs/proy_hydra_flamagas/dts/update_20170705_tablas_enblanco",
+    "C:/xampp/htdocs/proy_hydra_flamagas/dts/update_20170706_campo_en_knb1",
+    "C:/xampp/htdocs/proy_hydra_flamagas/dts/Datos/IN/BackUP"
  * 
 */
+include("vendor/theframework/components/autoload.php");
 //archivos .XNT que nos han proporcionado
 $oComp = new \TheFramework\Components\ComponentScandir();
+
+/*
+ *  "C:/xampp/htdocs/proy_hydra_flamagas/dts/update_20170620_pricing",
+    "C:/xampp/htdocs/proy_hydra_flamagas/dts/update_20170705_tablas_enblanco",
+    "C:/xampp/htdocs/proy_hydra_flamagas/dts/update_20170706_campo_en_knb1",
+    "C:/xampp/htdocs/proy_hydra_flamagas/dts/Datos/IN/BackUP"
+ */
 $oComp->run();
 
  
