3ac9a9bb5998bf0a1a8cfc05325f94fb5987de8f
eacevedof <eacevedof@yahoo.es>
2018-03-31 19:45:59 +0200

    comp_mssql_export.php 1.0.0

--- examples/components/comp_mssql_export.php
+++ examples/components/comp_mssql_export.php
@@ -5,7 +5,7 @@
  * Ejemplos: ComponentMssqlExport->get_insert_bulk(...) y ComponentMssqlExport->get_schema()
 */
 include("vendor/theframework/components/autoload.php");
-ini_set('max_execution_time',3000);
+ini_set("max_execution_time",3000);
 use TheFramework\Components\Db\Integration\ComponentMssqlExport;
 $arConn["server"]="localhost\MSSQLSERVER2014";
 $arConn["database"]="theframework";
