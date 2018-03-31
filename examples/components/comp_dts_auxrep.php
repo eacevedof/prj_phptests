ce1bac894766419544540e48bce312bd99bf2b04
eacevedof <eacevedof@yahoo.es>
2018-03-31 17:19:06 +0200

    comp_dts_auxrep 1.0.0

--- examples/components/comp_dts_auxrep.php
+++ examples/components/comp_dts_auxrep.php
@@ -1,6 +1,7 @@
 <?php
 //file: comp_gd2.php 1.0.0
 /*
+ * @file: comp_dts_auxrep 1.0.0
  * @info: proyecto Flamagas
  * Lee C:/xampp/htdocs/dts_flamagas_prod/interfaz obteniendo todos los archivos .dtsx
  * recupera su contenido y remplaza (STATUS='T',0,9)) en un nuevo archivo
@@ -9,7 +10,12 @@
 include("vendor/theframework/components/autoload.php");
 use TheFramework\Components\ComponentDtsxrep;
 $oComp = new ComponentDtsxrep();
+
+//cambia las tablas erp_archivo por erp_archivo_aux
+//guarda el archivo remplazado con ok_<nombre-archivo>
 $oComp->replace();
+
+//añade la condicion IIF($sTable.STATUS='T' a todos los archivos
 $oComp->replace_status();
  
 
