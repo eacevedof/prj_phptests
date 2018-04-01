cf606692f6d4ddd7174476122d9f36be0bc6290a
eacevedof <eacevedof@yahoo.es>
2018-04-01 17:11:50 +0200

    component_mssql.php v1.4.1 $oPdo->setAttribute(\PDO::SQLSRV_ATTR_ENCODING,\PDO::SQLSRV_ENCODING_UTF8);

--- vendor/theframework/components/db/component_mssql.php
+++ vendor/theframework/components/db/component_mssql.php
@@ -3,7 +3,7 @@
  * @author Eduardo Acevedo Farje.
  * @link www.eduardoaf.com
  * @name TheFramework\Components\Db\ComponentMssql 
- * @file component_mssql.php v1.4.0.B
+ * @file component_mssql.php v1.4.1
  * @date 19-09-2017 04:56 SPAIN
  * @observations
  */
@@ -28,7 +28,6 @@ class ComponentMssql
         $arConn["sqlsrv:Server"] = (isset($this->arConn["server"])?$this->arConn["server"]:"");
         $arConn["Database"] = (isset($this->arConn["database"])?$this->arConn["database"]:"");
         $arConn["ConnectionPooling"] = (isset($this->arConn["pool"])?$this->arConn["pool"]:"0");
-        $arConn["charset"] = (isset($this->arConn["charset"])?$this->arConn["charset"]:"UTF-8");
         
         $sString = "";
         foreach($arConn as $sK=>$sV)
@@ -44,7 +43,8 @@ class ComponentMssql
             $sConn = $this->get_conn_string();
             //https://stackoverflow.com/questions/38671330/error-with-php7-and-sql-server-on-windows
             $oPdo = new \PDO($sConn,$this->arConn["user"],$this->arConn["password"]);
-            $oPdo->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION );  
+            $oPdo->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION ); 
+            $oPdo->setAttribute(\PDO::SQLSRV_ATTR_ENCODING,\PDO::SQLSRV_ENCODING_UTF8);
             //$oPdo->setAttribute(\PDO::SQLSRV_ATTR_ENCODING, \PDO::SQLSRV_ENCODING_SYSTEM);
             $oCursor = $oPdo->query($sSQL);
             if($oCursor===FALSE)
