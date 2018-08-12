3cca1e21f92fc6064a02d95f984f89de770b6b09
eacevedof <eacevedof@yahoo.es>
2018-08-12 14:07:14 +0200

    cambios y pruebas en help_date

--- examples/helpers/help_date.php
+++ examples/helpers/help_date.php
@@ -1,5 +1,5 @@
 <?php
-//file: helpers.php 1.0.0
+//file: helpers.php 1.0.1
 /*
  * NOTES:
  * testing of TheFramework\Helpers\
@@ -12,24 +12,15 @@
  **/
 include_once("vendor/autoload.php");
 
-/*
-<div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
- 
+//<input type="date" id="datBirthdate" name="datBirthdate" as="date" data-options='{"useClearButton":true}'>
 
 use TheFramework\Helpers\HelperLabel;
 use TheFramework\Helpers\HelperDate;
 
 $oLabel = new HelperLabel("datBirthdate","Birthdate","lblBirthdate");
 $oDate = new HelperDate();
-$oDate->set_type("date");
 $oDate->set_id("datBirthdate");
 $oDate->set_name("datBirthdate");
 $oDate->show();
-*/
-
 
 //</editor-fold>
