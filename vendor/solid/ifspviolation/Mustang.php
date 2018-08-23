7e9d80758afeac44af3f503a598b0c0ba68593f5
eacevedof <eacevedof@yahoo.es>
2018-08-23 15:44:22 +0200

    cambio de nombre de carpeta de If a if

---
+++ vendor/solid/ifspviolation/Mustang.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name Solid\IfspViolation\Mustang
+ * @file Mustang.php
+ * @version 1.0.0
+ * @date 18-08-2018 20:19
+ * @observations
+ *  SOLID en Java
+ *  Tutorial: https://www.youtube.com/watch?v=j_ZnM8FJcmA
+ *  Ejemplos: https://android.jlelse.eu/solid-principles-the-definitive-guide-75e30a284dea
+ */
+namespace Solid\IfspViolation;
+
+use Solid\IfspViolation\IfCar;
+
+class Mustang implements IfCar
+{
+    public function start_engine()
+    {
+        //start engine
+    }
+    
+    public function accelerate()
+    {
+        //accelerate
+    }
+    
+    public function back_to_past() 
+    {
+        throw new Exception("UnsupportedOperationException");
+    }
+    
+    public function back_to_the_future() 
+    {
+        throw new Exception("UnsupportedOperationException");
+    }   
+    
+}//Mustang
