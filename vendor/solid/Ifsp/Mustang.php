2f9f346a1b21d776b758fa4e233a86a3edc2359d
eacevedof <eacevedof@yahoo.es>
2018-08-23 15:14:07 +0200

    La solucion a IfspViolation pasa por crear Solid\Ifsp\IfTimeMachine que encapsule los metodos que son solo del Delorean

---
+++ vendor/solid/Ifsp/Mustang.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name Solid\Ifsp\Mustang
+ * @file Mustang.php
+ * @version 1.0.0
+ * @date 18-08-2018 20:19
+ * @observations
+ *  SOLID en Java
+ *  Tutorial: https://www.youtube.com/watch?v=j_ZnM8FJcmA
+ *  Ejemplos: https://android.jlelse.eu/solid-principles-the-definitive-guide-75e30a284dea
+ */
+namespace Solid\Ifsp;
+
+use Solid\Ifsp\IfCar;
+
+class Mustang implements IfCar
+{
+    public function start_engine()
+    {
+        //start engine
+    }
+    
+    public function accelerate()
+    {
+        //accelerate
+    }
+    
+}//Mustang
