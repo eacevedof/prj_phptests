28a8d6f32e61e8152979be187c6db3ccd49273e9
eacevedof <eacevedof@yahoo.es>
2018-08-23 15:45:09 +0200

    dipviolation - ns DipViolation

---
+++ vendor/solid/dipviolation/Product.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name Solid\Dip\Product
+ * @file Product.php
+ * @version 1.0.0
+ * @date 18-08-2018 20:19
+ * @observations
+ *  SOLID en Java
+ *  Tutorial: https://www.youtube.com/watch?v=j_ZnM8FJcmA
+ *  Ejemplos: https://android.jlelse.eu/solid-principles-the-definitive-guide-75e30a284dea
+ */
+namespace Solid\Dip;
+
+class Product 
+{
+
+
+}//Product
