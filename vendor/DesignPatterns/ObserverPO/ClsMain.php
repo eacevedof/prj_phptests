c36c6d9095a6606a17e109ca88fc933b2ae3882c
eacevedof <eacevedof@yahoo.es>
2018-08-27 13:50:39 +0200

    observerpo file ClsMain.php version 1.0.1

--- vendor/DesignPatterns/ObserverPO/ClsMain.php
+++ vendor/DesignPatterns/ObserverPO/ClsMain.php
@@ -4,10 +4,10 @@
  * @link www.eduardoaf.com
  * @name DesignPatterns\ObserverPO
  * @file ClsMain.php
- * @version 1.0.0
+ * @version 1.0.1
  * @date 27-08-2018 12:56
  * @observations
- *  Ejemplo Post Office: [Post Office](https://youtu.be/rWvXJo3OAzs?t=285)
+ *  Ejemplo [Post Office Main Class](https://youtu.be/rWvXJo3OAzs?t=285)
  */
 namespace DesignPatterns\ObserverPO;
 
@@ -20,10 +20,10 @@ class ClsMain
         
         //mailbox 1 comprueba si tiene email
         $oPostOff->add_observer($oMailBox1);
-        //pregunta si hay email y si hay muestra por pantalla You have mail
+        //pregunta si hay email y si hay muestra por pantalla You have mail (oMailbox.update())
         $oPostOff->new_mail();
-        //
+        //deja de observar
         $oPostOff->remove_observer($oMailBox1);
-    }
+    }//main
     
 }//ClsMain
