fb64d3f2960f037f11fa58a78f3e5aa32671c37f
eacevedof <eacevedof@yahoo.es>
2018-08-27 13:56:00 +0200

    pattern_observerpo.php

---
+++ examples/mixed/pattern_observerpo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * pattern_observerpo.php
+ * [Observer Pattern - Design Pattern](https://www.youtube.com/watch?v=rWvXJo3OAzs)
+ */
+include_once("vendor/autoload.php");
+
+use DesignPatterns\ObserverPO\ClsMain;
+
+ClsMain::main();
