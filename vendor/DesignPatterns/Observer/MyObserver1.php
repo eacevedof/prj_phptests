06189ecf5c719cac0ee2e8409f4b2d5dae73bc9c
eacevedof <eacevedof@yahoo.es>
2018-08-27 12:21:53 +0200

    Comentarios de ejemplo Post Office

--- vendor/DesignPatterns/Observer/MyObserver1.php
+++ vendor/DesignPatterns/Observer/MyObserver1.php
@@ -8,7 +8,9 @@
  * @date 24-08-2018 20:19
  * @observations
  *  Patrones de diseño en laravel 
- *  Tutorial: https://youtu.be/SCpigk7UToM?t=1037
+ *  Tutorial: [Análisis de los patrones de diseño con larave](https://youtu.be/SCpigk7UToM?t=1037)
+ *  Ejemplo: [El interfaz SplSubject](http://php.net/manual/es/class.splsubject.php)
+ *  Ejemplo Post Office: [Post Office](https://www.youtube.com/watch?v=rWvXJo3OAzs)
  */
 namespace DesignPatterns\Observer;
 
