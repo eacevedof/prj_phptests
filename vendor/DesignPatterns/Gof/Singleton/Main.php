49be4354460aa695b3c9df0e0df4769cdaf78e07
eacevedof <eacevedof@yahoo.es>
2018-08-29 14:06:23 +0200

    cambio nombre

---
+++ vendor/DesignPatterns/Gof/Singleton/Main.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name DesignPatterns\Gof\Singleton\Main
+ * @file Main.php
+ * @version 1.0.0
+ * @date 29-08-2018 14:04
+ * @observations
+ *  Ejemplo []()
+ */
+namespace DesignPatterns\Gof\Singleton;
+
+class Main 
+{
+    public static function main(Array $arArgs=[])
+    {
+
+        
+        \dg::p("Main.main() executed!!");
+    }//main
+    
+}//Main
