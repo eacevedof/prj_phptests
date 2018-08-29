cc03784f0178eecc2bffe5164941ddea0badc015
eacevedof <eacevedof@yahoo.es>
2018-08-29 13:24:45 +0200

    GOF - ns Singleton

---
+++ vendor/DesignPatterns/Gof/Singleton/ClsMain.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name DesignPatterns\Gof\Singleton
+ * @file ClsMain.php
+ * @version 2.0.0
+ * @date 27-08-2018 14:12
+ * @observations
+ *  Ejemplo []()
+ */
+namespace DesignPatterns\Gof\Singleton;
+
+class ClsMain 
+{
+    public static function main(Array $arArgs=[])
+    {
+
+        
+        \dg::p("ClsMain.main() executed!!");
+    }//main
+    
+}//ClsMain
