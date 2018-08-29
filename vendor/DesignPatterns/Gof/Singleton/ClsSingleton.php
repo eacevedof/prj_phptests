6b0f77e27f181ec0b65c9b01ff2eac2766ddcd78
eacevedof <eacevedof@yahoo.es>
2018-08-29 13:24:45 +0200

    GOF - ns Singleton

---
+++ vendor/DesignPatterns/Gof/Singleton/ClsSingleton.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name DesignPatterns\Gof\Singleton
+ * @file ClsSingleton.php
+ * @version 1.1.0
+ * @date 27-08-2018 12:56
+ * @observations
+ *  Ejemplo []()
+ */
+namespace DesignPatterns\Gof\Singleton;
+class ClsSingleton
+{
+    
+}//ClsSingleton
