7bbc6edd3adf8820c9549fc402506610495855f7
eacevedof <eacevedof@yahoo.es>
2018-08-29 14:59:00 +0200

    Singleton finalizado

---
+++ examples/designpatterns/gof_singleton.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file: gof_singleton.php
+ * @info: https://github.com/eacevedof/prj_phptests/tree/master/vendor/DesignPatterns/Gof
+ *        [Curso de Patrones de diseño - 2 Singleton by MitoCode](https://www.youtube.com/watch?v=gocJeOHtj9w&list=PLvimn1Ins-41Uiugt1WbpyFo1XT1WOquL&index=2)
+ */
+include_once("vendor/autoload.php");
+
+use DesignPatterns\Gof\Singleton\Main;
+Main::main();
