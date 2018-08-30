5bea8afd0151d1efae4be5d29b9e10194079b638
eacevedof <eacevedof@yahoo.es>
2018-08-30 17:35:58 +0200

    var_dump de $_GET y $_POST

---
+++ examples/mixed/php_globals.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file: php_globals.php
+ * @info: var_dump de $_GET y $_POST
+ */
+
+\dg::p($_GET,"_GET");
+\dg::p($_POST,"_POST");
+//\dg::p($_SESSION,"_SESSION");
+//\dg::p($_SERVER,"_SESSION");
