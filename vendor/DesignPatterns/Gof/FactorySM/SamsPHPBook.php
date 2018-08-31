99791cd72bfa7c644d802694b2a0efbcf87629eb
eacevedof <eacevedof@yahoo.es>
2018-08-31 13:41:15 +0200

    El ejemplo de sourcemaking funciona!

---
+++ vendor/DesignPatterns/Gof/FactorySM/SamsPHPBook.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name DesignPatterns\Gof\FactorySM\SamsPHPBook 
+ * @file SamsPHPBook .php
+ * @version 1.0.0
+ * @date 29-08-2018 13:59
+ * @observations
+ *  Ejemplo []()
+ */
+namespace DesignPatterns\Gof\FactorySM;
+
+use DesignPatterns\Gof\FactorySM\AbstractPHPBook;
+
+class SamsPHPBook extends AbstractPHPBook {
+    private $author;
+    private $title;
+    function __construct() {
+        //alternate randomly between 2 books
+        mt_srand((double)microtime()*10000000);
+        $rand_num = mt_rand(0,1);      
+ 
+        if (1 > $rand_num) {
+            $this->author = 'George Schlossnagle';
+            $this->title  = 'Advanced PHP Programming';
+        } else {
+            $this->author = 'Christian Wenz';
+            $this->title  = 'PHP Phrasebook'; 
+        }  
+    }
+    function getAuthor() {return $this->author;}
+    function getTitle() {return $this->title;}
+}//SamsPHPBook
