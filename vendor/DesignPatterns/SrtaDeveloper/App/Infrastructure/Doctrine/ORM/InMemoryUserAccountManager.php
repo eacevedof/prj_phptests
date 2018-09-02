cb9bca78a007b27c4bda0474ea0f443bcd257fd6
eacevedof <eacevedof@yahoo.es>
2018-09-02 21:59:11 +0200

    DesignPatterns\SrtaDeveloper\App\Infrastructure\Doctrine\ORM\InMemoryUserAccountManager

---
+++ vendor/DesignPatterns/SrtaDeveloper/App/Infrastructure/Doctrine/ORM/InMemoryUserAccountManager.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name DesignPatterns\SrtaDeveloper\App\Infrastructure\Doctrine\ORM\InMemoryUserAccountManager
+ * @file InMemoryUserAccountManager.php
+ * @version 1.0.0
+ * @date 02-09-2018 17:21
+ * @observations
+ *  Ejemplo []()
+ */
+namespace DesignPatterns\SrtaDeveloper\App\Infrastructure\Doctrine\ORM;
+
+use DesignPatterns\SrtaDeveloper\App\Domain\UserAccountRepositoryInterface;
+use DesignPatterns\SrtaDeveloper\App\Domain\UserAccountFactoryInterface;
+use DesignPatterns\SrtaDeveloper\App\Domain\UserAccount;
+
+
+class InMemoryUserAccountManager implements UserAccountRepositoryInterface, UserAccountFactoryInterface
+{
+    private $userAccounts;
+
+    public function __construct()
+    {
+        $this->userAccounts = array();
+    }
+
+    public function findByEmail($email)
+    {
+        //...
+    }
+
+}//InMemoryUserAccountManager
