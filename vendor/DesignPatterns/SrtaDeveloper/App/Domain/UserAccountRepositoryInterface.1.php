127582fc216297e981e01a556ecc07171eb25be6
eacevedof <eacevedof@yahoo.es>
2018-09-02 20:53:50 +0200

    repo y factoriy interface

---
+++ vendor/DesignPatterns/SrtaDeveloper/App/Domain/UserAccountRepositoryInterface.1.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name DesignPatterns\SrtaDeveloper\App\Domain\UserAccountRepositoryInterface
+ * @file UserAccountRepositoryInterface.php
+ * @version 1.0.0
+ * @date 02-09-2018 17:21
+ * @observations
+ *  Ejemplo []()
+ */
+namespace DesignPatterns\SrtaDeveloper\App\Domain;
+
+interface UserAccountRepositoryInterfaceRepositoryInterface
+{
+    public function findById($id);
+    public function findByEmail($id);
+    public function add(UserAccount $userAccount);
+
+}//UserAccountRepositoryInterface
