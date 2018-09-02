d4832c73836f2b71589ddda13c95e0c4558bd44b
eacevedof <eacevedof@yahoo.es>
2018-09-02 20:42:19 +0200

    clases DesignPatterns\SrtaDeveloper\App\Domain\Event\UserAccountRegisteredEvent y DesignPatterns\SrtaDeveloper\App\Infrastructure\Doctrine\ORM\DoctrineORMUserAccount

---
+++ vendor/DesignPatterns/SrtaDeveloper/App/Infrastructure/Doctrine/ORM/DoctrineORMUserAccount.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name DesignPatterns\SrtaDeveloper\App\Infrastructure\Doctrine\ORM\DoctrineORMUserAccount
+ * @file DoctrineORMUserAccount.php
+ * @version 1.0.0
+ * @date 02-09-2018 17:21
+ * @observations
+ *  Ejemplo []()
+ */
+namespace DesignPatterns\SrtaDeveloper\App\Infrastructure\Doctrine\ORM;
+
+use DesignPatterns\SrtaDeveloper\App\Domain\UserAccount;
+
+class DoctrineORMUserAccount extends UserAccount
+{
+    public function doStuffOnPrePersist()
+    {
+
+    }//doStuffOnPrePersist
+
+    public function doStuffOnPostPersist()
+    {
+
+    }//doStuffOnPostPersist    
+
+}//DoctrineORMUserAccount
