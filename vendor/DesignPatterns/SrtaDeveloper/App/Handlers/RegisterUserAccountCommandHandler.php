30c2424c08c7728dc9d4f3d25a80036158791703
eacevedof <eacevedof@yahoo.es>
2018-09-02 19:43:16 +0200

    DesignPatterns\App\Handler\RegisterUserAccountCommandHandler

---
+++ vendor/DesignPatterns/SrtaDeveloper/App/Handlers/RegisterUserAccountCommandHandler.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name DesignPatterns\App\Handler\RegisterUserAccountCommandHandler
+ * @file RegisterUserAccountCommandHandler.php
+ * @version 1.0.0
+ * @date 02-09-2018 17:21
+ * @observations
+ *  Ejemplo []()
+ */
+namespace DesignPatterns\SrtaDeveloper\App\Handler;
+
+class RegisterUserAccountCommandHandler implements CommandInterface
+{
+    private $factory;
+    private $repository;
+
+    public function __construct(UserAccountFactoryInterface $factory, UserAccountRepositoryInterface $repository)
+    {
+        $this->factory = $factory;
+        $this->repository = $repository;
+
+    }//_construct
+
+    public function handle(RegisterUserAccountCommand $command)
+    {
+        $userAccount = $this->factory->createUserAccount(
+            $command->getEmail(),
+            $command->getPassword(),
+            $command->getFirstName(),
+            $command->getLastName()
+        );
+        $this->repository->add($userAccount);
+    }//handle
+
+}//RegisterUserAccountCommandHandler
