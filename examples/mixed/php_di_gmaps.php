25a0433c21a6935972651a8109e3292c65690f45
Eduardo A.F <eacevedof@yahoo.es>
2019-10-06 17:05:20 +0200

    la inyección manual

--- examples/mixed/php_di_gmaps.php
+++ examples/mixed/php_di_gmaps.php
@@ -4,20 +4,20 @@
  * @info: di googlemaps http://php-di.org/doc/understanding-di.html
  */
 include "vendor/autoload.php";
-class GoogleMaps
+class GoogleMaps_Old
 {
     public function getCoordinatesFromAddress($address) {
         // calls Google Maps webservice
     }
 }
-class OpenStreetMap
+class OpenStreetMap_Old
 {
     public function getCoordinatesFromAddress($address) {
         // calls OpenStreetMap webservice
     }
 }
 
-class StoreService
+class StoreService_Old
 {
     public function getStoreCoordinates($store) {
         $oGmaps = new GoogleMaps();
@@ -26,3 +26,52 @@ class StoreService
     }
 }
 
+//hay que desacoplar el código anterior
+//Paso 1: interfaz
+interface InterfaceGeolocationService 
+{
+    public function getCoordinatesFromAddress($address);
+}
+
+class IdentityStore
+{
+    public function getAddress()
+    {
+        return "this is an IdentityStore.address | ";
+    }
+}
+
+class GoogleMaps implements InterfaceGeolocationService
+{
+    public function getCoordinatesFromAddress($address)
+    {
+        echo "GoogleMaps.getCoordinatesFromAddress: | $address ";
+    }
+}
+
+class OpenStreetMap implements InterfaceGeolocationService
+{
+    public function getCoordinatesFromAddress($address)
+    {
+        echo "OpenStreetMap.getCoordinatesFromAddress: $address |";
+    }    
+}
+
+class StoreService
+{
+    private $oIGeoService;
+    
+    //$oIGeoService puede ser: GoogleMaps o OpenStreetMap
+    public function __construct(InterfaceGeolocationService $oIGeoService) {
+        $this->oIGeoService = $oIGeoService;
+    }
+    
+    public function getStoreCoordinates($store)
+    {
+        return $this->oIGeoService->getCoordinatesFromAddress($store->getAddress());
+    }
+}
+
+$oGmaps = new GoogleMaps();
+$storeservice = new StoreService($oGmaps);
+$storeservice->getStoreCoordinates(new IdentityStore());
