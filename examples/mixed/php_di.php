a02367ff9eec513adf61c8fccdf9d87e70abf469
Eduardo A.F <eacevedof@yahoo.es>
2019-10-06 16:16:26 +0200

    inyección manual

--- examples/mixed/php_di.php
+++ examples/mixed/php_di.php
@@ -3,4 +3,34 @@
  * @file: php_di.php
  * @info: ensayos con el inyector de dependencias php-di 6.0: http://php-di.org/
  */
-echo "hola php_di.php";
+include("vendor/autoload.php");
+
+class Mailer 
+{
+    public function mail($recipient, $content)
+    {
+        echo "<pre/> mail: {recipient: $recipient, content: $content} sent";
+    }
+}
+
+class UserManager
+{
+    private $mailer;
+    
+    public function __construct(Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+    
+    public function register($email,$password)
+    {
+        //check user register
+        $this->mailer->mail($email,"Hello and Welcome your pass is $password!");
+    }
+}
+
+//tipical approach
+$mailer = new Mailer();
+$usermanager = new UserManager($mailer);
+$usermanager->register("desti@mai.com", "123");
+
