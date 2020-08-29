44d4cd62efe950f8ee09627f1aab0354a80302fd
ioedumac <eacevedof@yahoo.es>
2020-08-29 19:43:03 +0200

    quito sleep 1

--- ports.sh
+++ ports.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 for i in {1..65536}
 do
-  echo "port $i"
+  # echo "port $i"
   php -S localhost:$i -t ./public
   PID=$!
-  # Wait for 2 seconds
-  sleep 1
+
+  sleep 0.1
   # Kill it
   kill $PID
 done
