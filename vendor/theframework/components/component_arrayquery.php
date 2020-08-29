ca4b099bd36669e91010fb3892fc7ee1aee37938
ioedumac <eacevedof@yahoo.es>
2020-08-29 10:46:26 +0200

    component_arrayquery

---
+++ vendor/theframework/components/component_arrayquery.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @version 1.0.0
+ * @name ComponentArrayquery
+ * @file component_arrayquery.php
+ * @date 29-08-2020 16:14 (SPAIN)
+ * @observations:
+ */
+namespace TheFramework\Components;
+
+class ComponentArrayquery
+{
+    private $array;
+    
+    public function __construct(array $array)
+    {
+        $this->array = $array;
+    }
+    
+    
+}
