00e84a2d9369b1a88d94ba80d64c9a2011daff07
ioedumac <eacevedof@yahoo.es>
2020-12-30 21:50:18 +0100

    test con resultados en comentarios

--- examples/components/comp_arrayquery.php
+++ examples/components/comp_arrayquery.php
@@ -7,6 +7,7 @@
 
 include(TFW_PATHROOTDS."vendor/theframework/components/autoload.php");
 use TheFramework\Components\ComponentArrayquery;
+//supongamos que un endpoint me devuelve esto
 $ar1 = [
     ["id"=>1,"description"=>"some description 1 x","price"=>10.00,"date"=>"20100220"],
     ["id"=>2,"description"=>"some description 2 y","price"=>20.22,"date"=>"20221001"],
@@ -20,6 +21,7 @@ $ar1 = [
     ["id"=>10,"description"=>"","price"=>13.99,"date"=>"19900228"],
 ];
 
+//otra llamada me devuelve lo siguiente
 $ar2 = [
     ["id"=>1,"description"=>"some description 1 a","price"=>10.00,"date"=>"20100220"],
     ["id"=>2,"description"=>"some description 2 y","price"=>20.22,"date"=>"20221001"],
@@ -32,31 +34,143 @@ $ar2 = [
     ["id"=>9,"description"=>null,"price"=>7.65,"date"=>"20100228"],
     ["id"=>11,"description"=>"","price"=>13.99,"date"=>"19900228"],
 ];
-//array_reduce()
+
 $oComp = new ComponentArrayquery($ar1);
+
+//probando map, filter reduce
+/*
 $r = $oComp->map(function($item){
         return [
-          "id" => $item["id"], "name"=> $item["description"], "day"=>$item["date"], "p"=>$item["price"]
+            //transformo el array original, cambio date por day y price por p
+            "id" => $item["id"], "name"=> $item["description"], "day"=>$item["date"], "p"=>$item["price"]
         ];
     })
     ->filter(function ($item){
+        //me quedo con los precios > 10
         return $item["p"] > 10;
     })
     ->reduce(function($ac, $item){
+        //sumo los precios
         return $ac + $item["p"];
     },0)
 ;
+*/
 
 //$r = $oComp->remove_column(["id"])->distinct()->where("price",20.22);
+/**
+array (
+    0 =>
+    array (
+        'description' => 'some description 2 y',
+        'price' => 20.22,
+        'date' => '20221001',
+    ),
+    1 =>
+    array (
+        'description' => 'some description 3 z',
+        'price' => 20.22,
+        'date' => '20221001',
+    ),
+)
+ */
 //$r = $oComp->distinct()->where("price","20.2%","like")->where("date","%05","like" );
-//$r = $oComp->distinct()->where("description","%z%","like");
+/*
+array (
+  0 =>
+  array (
+    'id' => 7,
+    'description' => 'some description 6 z',
+    'price' => 20.25,
+    'date' => '20221105',
+  ),
+)
+ */
+$r = $oComp->distinct()->where("description","%z%","like");
+/*
+array (
+  0 =>
+  array (
+    'id' => 3,
+    'description' => 'some description 3 z',
+    'price' => 20.22,
+    'date' => '20221001',
+  ),
+  1 =>
+  array (
+    'id' => 6,
+    'description' => 'some description 3 z',
+    'price' => 20.22,
+    'date' => '20221001',
+  ),
+  2 =>
+  array (
+    'id' => 7,
+    'description' => 'some description 6 z',
+    'price' => 20.25,
+    'date' => '20221105',
+  ),
+)
+*/
 //$r = $oComp->distinct()->where("price",6,">")->where("price",11,"<");
 //$r = $oComp->is_null("description");
 //$r = $oComp->is_empty("description");
 //$r = $oComp->in("date", ["20221001","20221105"])->not_in("description", ["some description 2 y"]);
 
 //$r = $oComp->innerjoin($ar2,["id"=>"id","description"=>"description"])->orderby("date", "desc");
-//$r = $oComp->leftjoin($ar2,["id"=>"id","description"=>"description"])->orderby("date", "desc");
+/*
+ array (
+  0 =>
+  array (
+    'id' => 7,
+    'description' => 'some description 6 z',
+    'price' => 20.25,
+    'date' => '20221105',
+  ),
+  1 =>
+  array (
+    'id' => 3,
+    'description' => 'some description 3 z',
+    'price' => 20.22,
+    'date' => '20221001',
+  ),
+  2 =>
+  array (
+    'id' => 6,
+    'description' => 'some description 3 z',
+    'price' => 20.22,
+    'date' => '20221001',
+  ),
+)
+*/
+$r = $oComp->leftjoin($ar2,["id"=>"id","description"=>"description"])->orderby("date", "desc");
+/*
+array (
+  0 =>
+  array (
+    'id' => 7,
+    'description' => 'some description 6 z',
+    'price' => 20.25,
+    'date' => '20221105',
+    '*leftjoin*' => 0,
+  ),
+  1 =>
+  array (
+    'id' => 3,
+    'description' => 'some description 3 z',
+    'price' => 20.22,
+    'date' => '20221001',
+    '*leftjoin*' => 0,
+  ),
+  2 =>
+  array (
+    'id' => 6,
+    'description' => 'some description 3 z',
+    'price' => 20.22,
+    'date' => '20221001',
+    '*leftjoin*' => 0,
+  ),
+)
+*/
 pr($r->get_result(),"result");
 
 
