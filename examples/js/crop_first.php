9052febe8a64cd902f6246d6522b5a1b739b9b2e
ioedumac <eacevedof@yahoo.es>
2021-04-05 19:28:01 +0200

    crop-first con bootstrap4

--- examples/js/crop_first.php
+++ examples/js/crop_first.php
@@ -9,9 +9,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Crop before</title>
-    <script src="/js/cropper-js/cropper.js"></script>
-    <script src="/js/cropper-js/cropper.css"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="/js/cropper-js/cropper.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
+
+<script src="/js/cropper-js/cropper.js"></script>
 </body>
 </html>
