68462413c9b11ae6d3d0bd6b07ece78b57ddf334
ioedumac <eacevedof@yahoo.es>
2021-04-05 19:29:10 +0200

    crop-first con bootstrap5

--- examples/js/crop_first.php
+++ examples/js/crop_first.php
@@ -7,6 +7,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <title>Crop before</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
