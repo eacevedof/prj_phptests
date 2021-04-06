e64fb8273012308bb858087a061955c508cd0bef
ioedumac <eacevedof@yahoo.es>
2021-04-06 21:01:11 +0200

    crop-first

--- examples/js/crop_first.php
+++ examples/js/crop_first.php
@@ -3,6 +3,18 @@
  * @file: crop_first 2.0.0
  * @info: Crop antes de subir una imagen
  */
+
+if($_POST["image"] ?? null) {
+    $folderPath = 'upload/';
+    $image_parts = explode(";base64,", $_POST['image']);
+    $image_type_aux = explode("image/", $image_parts[0]);
+    $image_type = $image_type_aux[1];
+    $image_base64 = base64_decode($image_parts[1]);
+    $file = $folderPath . uniqid() . '.png';
+    file_put_contents($file, $image_base64);
+    echo json_encode(["image uploaded successfully."]);
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,14 +27,109 @@
 </head>
 <body>
 <div class="container">
-    <h2>Upload images</h2>
-    <form class="">
+    <form class="" method="post">
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            <label for="fileImage" class="form-label">Upload Images</label>
+            <input type="file" name="image" id="fileImage" class="image">
         </div>
     </form>
 </div>
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalLabel">Crop image</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="img-container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <img id="image">
+                        </div>
+                        <div class="col-md-4">
+                            <div class="preview"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary" id="crop">Crop</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="/js/cropper-js/cropper.js"></script>
+<script>
+var bs_modal = $('#modal');
+var image = document.getElementById('image');
+var cropper,reader,file;
+
+
+$("body").on("change", ".image", function(e) {
+    var files = e.target.files;
+    var done = function(url) {
+        image.src = url;
+        bs_modal.modal('show');
+    };
+
+
+    if (files && files.length > 0) {
+        file = files[0];
+
+        if (URL) {
+            done(URL.createObjectURL(file));
+        } else if (FileReader) {
+            reader = new FileReader();
+            reader.onload = function(e) {
+                done(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    }
+});
+
+bs_modal.on('shown.bs.modal', function() {
+    cropper = new Cropper(image, {
+        aspectRatio: 1,
+        viewMode: 3,
+        preview: '.preview'
+    });
+}).on('hidden.bs.modal', function() {
+    cropper.destroy();
+    cropper = null;
+});
+
+$("#crop").click(function() {
+    canvas = cropper.getCroppedCanvas({
+        width: 160,
+        height: 160,
+    });
+
+    canvas.toBlob(function(blob) {
+        url = URL.createObjectURL(blob);
+        var reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = function() {
+            var base64data = reader.result;
+
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: "upload.php",
+                data: {image: base64data},
+                success: function(data) {
+                    bs_modal.modal('hide');
+                    alert("success upload image");
+                }
+            });
+        };
+    });
+});
+</script>
 </body>
 </html>
