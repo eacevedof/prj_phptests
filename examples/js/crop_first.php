8d82528291a48099d8bbc8459ef3804fd82328ae
ioedumac <eacevedof@yahoo.es>
2021-04-06 21:09:37 +0200

    crop-first esqueleto

--- examples/js/crop_first.php
+++ examples/js/crop_first.php
@@ -62,7 +62,16 @@ if($_POST["image"] ?? null) {
         </div>
     </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script
+        src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"
+></script>
+<script
+        src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
+        integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/"
+        crossorigin="anonymous"
+></script>
 <script src="/js/cropper-js/cropper.js"></script>
 <script>
 var bs_modal = $('#modal');
