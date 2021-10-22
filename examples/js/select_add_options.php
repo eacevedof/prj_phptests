05ef452154292354d3cb6de27e664f66c4de9884
ioedumac <eacevedof@yahoo.es>
2021-10-22 19:07:31 +0200

    slect_add_options.php

---
+++ examples/js/select_add_options.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * @file: select_add_options.php
+ * @info: agregar opciones con js a elemento select
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <title>add options to select element useing js</title>
+</head>
+<body>
+<form>
+
+</form>
+<script type="module">
+</script>
+<style></style>
+</body>
+</html>
