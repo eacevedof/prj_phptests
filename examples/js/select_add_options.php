cf38552de3c732fcb76c098506ae656eddf386dc
ioedumac <eacevedof@yahoo.es>
2021-10-22 19:21:02 +0200

    slect_add_options.php ok

--- examples/js/select_add_options.php
+++ examples/js/select_add_options.php
@@ -9,14 +9,35 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
-    <title>add options to select element useing js</title>
+    <title>Add options to select element useing js</title>
 </head>
 <body>
-<form>
-
+<h1>Add options to select element useing js</h1>
+<form method="post" action="">
+    <label for="sel-example">Example:</label><br/>
+    <select id="sel-example"></select>
 </form>
 <script type="module">
+const values = [
+  {id:1, text:"value 1"},
+  {id:2, text:"value 2"},
+  {id:3, text:"value 3"},
+  {id:4, text:"value 5"},
+]
+const $select = document.getElementById("sel-example")
+
+//empty option
+const $option = document.createElement("option")
+$option.value = ""
+$option.innerHTML = "Please choose an option :)"
+$select.appendChild($option)
+
+values.forEach(obj => {
+  const $option = document.createElement("option")
+  $option.value = obj.id
+  $option.innerHTML = obj.text
+  $select.appendChild($option)
+})
 </script>
-<style></style>
 </body>
 </html>
