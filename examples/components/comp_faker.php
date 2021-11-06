a2b27512d0b967af8dc4a171fee369a4599c88a7
ioedumac <eacevedof@yahoo.es>
2021-11-06 19:33:51 +0100

    componentfaker

--- examples/components/comp_faker.php
+++ examples/components/comp_faker.php
@@ -1,16 +1,18 @@
 <?php
 /*
- * @file: comp_rulez 1.0.0
+ * @file: comp_faker 1.0.0
  * @info:
  *
  * 
 */
-include(TFW_PATHROOTDS."vendor/rulez/bootstrap.php");
+include(TFW_PATHROOTDS."vendor/theframework/components/autoload.php");
 
 use TheFramework\Components\ComponentFaker;
 $service = new ComponentFaker();
 $data["fake_int"] = $service->get_int();
 $data["fake_int_max_15"] = $service->get_int(15);
 $data["fake_int_min_3_max_15"] = $service->get_int(15,3);
+
+pr($data,"faker result");
 
 
