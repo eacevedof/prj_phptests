494f213eee174cc9a7a2d070046f1ff022e66f65
ioedumac <eacevedof@yahoo.es>
2022-04-02 10:01:23 +0200

    EventSourcing

--- examples/eventsourcing/app/Domain/PostEntity.php
+++ examples/eventsourcing/app/Domain/PostEntity.php
@@ -3,8 +3,20 @@ namespace App\Publishing;
 
 final class PostEntity
 {
+    private int $id;
+
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
     public function publish()
     {
 
+    }
+
+    public function id(): int
+    {
+        return $this->id;
     }
 }
--- examples/eventsourcing/app/Domain/UserEntity.php
+++ examples/eventsourcing/app/Domain/UserEntity.php
@@ -3,8 +3,20 @@ namespace App\Publishing;
 
 final class UserEntity
 {
+    private int $id;
+
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
     public function publish()
     {
 
+    }
+
+    public function id(): int
+    {
+        return $this->id;
     }
 }
