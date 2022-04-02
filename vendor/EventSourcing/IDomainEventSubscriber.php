ef37d6685df7c03b77c685b9f4cff029ffdcc639
ioedumac <eacevedof@yahoo.es>
2022-04-02 20:06:26 +0200

    EventSourcing ok

--- vendor/EventSourcing/IDomainEventSubscriber.php
+++ vendor/EventSourcing/IDomainEventSubscriber.php
@@ -4,6 +4,5 @@ namespace EventSourcing;
 //https://youtu.be/uIu139WusKU?t=373
 interface IDomainEventSubscriber
 {
-    //public function isSubscribedTo(IDomainEvent $domainEvent): bool;
     public function handle(IDomainEvent $domainEvent): self;
 }
