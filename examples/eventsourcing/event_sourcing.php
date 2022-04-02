627348208e8edd05c611c05385531ddc5028ce66
ioedumac <eacevedof@yahoo.es>
2022-04-02 11:03:45 +0200

    EventSourcing

--- examples/eventsourcing/event_sourcing.php
+++ examples/eventsourcing/event_sourcing.php
@@ -5,9 +5,11 @@ use App\Publishing\Application\PublishCommandHandler;
 use App\Publishing\Domain\PostRepository;
 use App\Publishing\Domain\UserRepository;
 use EventSourcing\DomainEventPublisher;
+use App\Publishing\Domain\PublishPostCommand;
 
+$command = new PublishPostCommand(1, 1);
 $publisher = DomainEventPublisher::instance();
 (new PublishCommandHandler(
     new PostRepository(),
     new UserRepository()
-))->execute();
+))->execute($command);
