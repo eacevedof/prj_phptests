d08f1373929a3670bd899e38e1492dd220a6ffe3
ioedumac <eacevedof@yahoo.es>
2022-04-02 18:26:19 +0200

    EventSourcing

--- examples/eventsourcing/post_controller.php
+++ examples/eventsourcing/post_controller.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @file: post_controller.php
+ * @info: [Rigor Talks - Playlist](https://www.youtube.com/watch?v=aKcmbOZV9mA&list=PLfgj7DYkKH3Cd8bdu5SIHGYXh_bPV2idP&index=1)
+ */
 include_once("app/bootstrap.php");
 
 use App\Publishing\Application\PublishCommandHandler;
