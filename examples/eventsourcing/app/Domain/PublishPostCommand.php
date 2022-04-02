216cda1c6c9f1be3fc1f81e6de74759d911dbe15
ioedumac <eacevedof@yahoo.es>
2022-04-02 11:11:24 +0200

    EventSourcing

--- examples/eventsourcing/app/Domain/PublishPostCommand.php
+++ examples/eventsourcing/app/Domain/PublishPostCommand.php
@@ -1,6 +1,12 @@
 <?php
 namespace App\Publishing\Domain;
 
+/**
+ * https://youtu.be/6dwrRVt2wVg
+ * esta clase es un DTO que se podria serializar y logar
+ * Class PublishPostCommand
+ * @package App\Publishing\Domain
+ */
 final class PublishPostCommand
 {
     private int $postId;
