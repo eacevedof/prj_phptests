c5fc1491a60b464a0bccbdf5adef83bc5a2f6f07
ioedumac <eacevedof@yahoo.es>
2022-04-03 20:04:55 +0200

    EventSourcing ddd

--- examples/eventsourcing/app/Publishing/Application/LoggerDecorator.php
+++ examples/eventsourcing/app/Publishing/Application/LoggerDecorator.php
@@ -1,22 +1,24 @@
 <?php
 namespace App\Publishing\Application;
 
-use App\Publishing\Domain\Events\PostWasPublishedEvent;
+use App\Publishing\Application\Commands\ICommand;
 use App\Publishing\Domain\ICommandHandler;
 use App\Publishing\Domain\IEntity;
 use App\Publishing\Domain\PostRepository;
 use App\Publishing\Domain\UserRepository;
-use EventSourcing\IDomainEvent;
+use App\Publishing\Domain\Events\PostWasPublishedEvent;
 
 final class LoggerDecorator
 {
+    private ICommandHandler $commandHandler;
+
     public function __construct(ICommandHandler $commandHandler, $monolog)
     {
         $this->commandHandler = $commandHandler;
         $this->monolog = $monolog;
     }
 
-    public function execute(IDomainEvent $command): IEntity
+    public function execute(ICommand $command): IEntity
     {
         $this->monolog->log(serialize($command));
         return $this->commandHandler->execute($command);
