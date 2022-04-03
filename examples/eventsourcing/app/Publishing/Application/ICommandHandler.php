c0f925d60826c1f703a9923014034e4ef972cca1
ioedumac <eacevedof@yahoo.es>
2022-04-03 19:47:23 +0200

    EventSourcing ddd

--- examples/eventsourcing/app/Publishing/Application/ICommandHandler.php
+++ examples/eventsourcing/app/Publishing/Application/ICommandHandler.php
@@ -3,7 +3,5 @@ namespace App\Publishing\Application;
 
 interface ICommandHandler
 {
-    //no puedo definir execute pq el DTO varia segun sus metodos a ejecutar en el cuerpo de
-    //execute
     //public function execute($command): IEntity;
 }
