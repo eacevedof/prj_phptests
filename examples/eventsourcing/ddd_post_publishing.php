f50ec583ab2f1a6b1e1b39615bbe4466532df025
ioedumac <eacevedof@yahoo.es>
2022-04-24 20:54:40 +0200

    sin composer

--- examples/eventsourcing/ddd_post_publishing.php
+++ examples/eventsourcing/ddd_post_publishing.php
@@ -3,7 +3,6 @@
  * @file: ddd_post_publishing.php
  * @info: [Rigor Talks - Playlist](https://www.youtube.com/watch?v=aKcmbOZV9mA&list=PLfgj7DYkKH3Cd8bdu5SIHGYXh_bPV2idP&index=1)
  */
-include_once(TFW_PATHROOTDS."vendor/autoload.php");
 include_once("src/autoload.php");
 include_once("src/config/listeners/commands.php");
 include_once("src/config/listeners/events.php");
