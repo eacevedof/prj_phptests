986a43d78dd66fcfb17063a3fa03c892f8707462
ioedumac <eacevedof@yahoo.es>
2022-04-25 19:50:12 +0200

    tipos primitivos *Type.php

---
+++ examples/eventsourcing/src/Blog/Domain/Types/PostStatusType.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Blog\Domain\Types;
+
+use App\Shared\Domain\ValueObject\IntValueObject;
+
+final class PostStatusType extends IntValueObject
+{
+
+}
---
+++ examples/eventsourcing/src/Blog/Domain/Types/UserEmailType.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Blog\Domain\Types;
+
+use App\Shared\Domain\ValueObject\StringValueObject;
+
+final class UserEmailType extends StringValueObject
+{
+
+}
