7bc2bad283215bed3beda0d2c055a1fb47229190
ioedumac <eacevedof@yahoo.es>
2022-09-30 21:23:56 +0200

    dates

--- examples/mixed/php_dates_in_range.php
+++ examples/mixed/php_dates_in_range.php
@@ -9,3 +9,27 @@ $history = [
     ["start" => "", "end" => ""],
 
 ];
+
+final class IntersectHelper
+{
+    private array $request = ["start"=>"", "end" => ""];
+    private array $forecast = ["start"=>"", "end" => ""];
+
+    private array $result = [
+        "history" => ["start"=>"", "end" => ""],
+        "forecast" => ["start"=>"", "end" => ""],
+    ];
+
+    public function set_request(string $start, $end): self
+    {
+        $this->request = ["start" => $start, "end" => $end];
+        return $this;
+    }
+
+    public function set_forecast(string $start, $end): self
+    {
+        $this->forecast = ["start" => $start, "end" => $end];
+        return $this;
+    }
+
+}
