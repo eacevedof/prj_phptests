9ece5f2b722c83763287a74ce26ca5d37744bde7
ioedumac <eacevedof@yahoo.es>
2022-10-02 17:57:50 +0200

    php-dates

--- examples/mixed/php_dates_in_range.php
+++ examples/mixed/php_dates_in_range.php
@@ -9,12 +9,12 @@ final class IntersectHelper
 {
     private array $request = ["start"=>"", "end" => ""];
     private array $forecast = ["start"=>"", "end" => ""];
-    private array $history = ["start"=>"", "end" => ""];
+    //private array $history = ["start"=>"", "end" => ""];
 
     private array $result = [
         "request" => ["start"=>"", "end" => ""],
-        "history" => ["start"=>"", "end" => ""],
         "forecast" => ["start"=>"", "end" => ""],
+        "history" => ["start"=>"", "end" => ""],
     ];
 
     public function set_request(string $start, $end): self
@@ -29,10 +29,10 @@ final class IntersectHelper
         return $this;
     }
 
-    public function set_history(string $start, $end): self
+    private function set_history(string $start, $end): self
     {
-        $this->history = ["start" => $start, "end" => $end];
-        return $this;
+        //$this->history = ["start" => $start, "end" => $end];
+        //return $this;
     }
 
     public function get_calculated(): array
@@ -49,7 +49,7 @@ final class IntersectHelper
     {
         //si req-end >= for-end => hasta el fin del forecast history hasta req
         $today = date("Y-m-d");
-        if ($reqend = $this->request["end"] >= $forend = $this->forecast["end"]) {
+        if (($reqend = $this->request["end"]) >= ($forend = $this->forecast["end"])) {
             $this->result["forecast"]["end"] = $forend;
             $this->result["history"]["end"] = $reqend;
             if ($reqend>$today) {
@@ -70,7 +70,7 @@ final class IntersectHelper
 
     private function _calc_starts(): void
     {
-        if ($reqstart = $this->request["start"] <= $forstart = $this->forecast["start"]) {
+        if (($reqstart = $this->request["start"]) <= ($forstart = $this->forecast["start"])) {
             $this->result["forecast"]["start"] = $forstart;
             $this->result["history"]["start"] = $reqstart;
             return;
@@ -100,7 +100,7 @@ $forecast = [
 foreach($request as $req) {
     $r = (new IntersectHelper())
         ->set_request($req["start"], $req["end"])
-        ->set_history($history[0]["start"], $history[0]["end"])
+        //->set_history($history[0]["start"], $history[0]["end"])
         ->set_forecast($forecast[0]["start"], $forecast[0]["end"])
         ->get_calculated()
     ;
