c9cd7435cf45378be9200e91ae018f0277dc501e
ioedumac <eacevedof@yahoo.es>
2023-06-17 14:07:58 +0200

    composite

--- vendor/DesignPatterns/Upm/Composite/TreeNumbers.php
+++ vendor/DesignPatterns/Upm/Composite/TreeNumbers.php
@@ -7,5 +7,43 @@ use DesignPatterns\Upm\Composite\Number;
 final class TreeNumbers
 {
     private Number $number;
+    private array $treeNumbersList = [];
+
+
+    public function isComposite(): bool
+    {
+        return true;
+    }
+
+    public function add(TreeNumbers $treeNumbers): void
+    {
+        $this->treeNumbersList[] = $treeNumbers;
+    }
+
+    public function remove(TreeNumbers $treeNumbers): void
+    {
+        $this->treeNumbersList[] = $treeNumbers;
+    }
+
+    public function numberOfTreeNumbers(): int
+    {
+        return count($this->treeNumbersList);
+    }
+
+    public function sum(): int
+    {
+        return 0;
+    }
+
+    public function higher(): int
+    {
+        return 0;
+    }
+
+    public function toString(): string
+    {
+        return "";
+    }
+
 
 }
