b9dbac02ee27c05340f067605a745194ffa069e5
ioedumac <eacevedof@yahoo.es>
2023-06-18 15:30:27 +0200

    backup

--- examples/mixed/recursive_2.php
+++ examples/mixed/recursive_2.php
@@ -4,24 +4,31 @@
  * test recursive childs
  */
 
+ini_set("memory_limit", "256M");
+
 $data = [];
 for($i = 0; $i<100; $i++)
     $data[] = $i;
 
 for($i = 0; $i<33; $i=$i+2)
-    $data[$i] = [$i];
+    $data[$i] = ["a$i"];
 
 for($i = 33; $i<45; $i=$i+2)
-    $data[$i] = [[$i]];
+    $data[$i] = [["b$i"], ["c$i"]];
+
+//print_r($data);die;
 
 function printTree(array $tree): void
 {
-    foreach ($tree as $leaf) {
+    foreach ($tree as $i=>$leaf) {
         if (!is_array($leaf)) {
-            echo $leaf;
+            echo "$leaf, ";
             continue;
         }
-        printTree($tree);
+        else {
+            printTree($tree);
+        }
+        //printTree($tree);
     }
 }
 
