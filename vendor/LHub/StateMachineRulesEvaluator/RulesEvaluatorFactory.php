f1dcb8b87144a0be21a9db34457c396c1be32d57
eacevedof@mbpro2014 <eacevedof@>
2023-08-06 22:47:14 +0200

    backup

--- vendor/LHub/StateMachineRulesEvaluator/RulesEvaluatorFactory.php
+++ vendor/LHub/StateMachineRulesEvaluator/RulesEvaluatorFactory.php
@@ -9,7 +9,9 @@ final class RulesEvaluatorFactory
     public function getEvaluatorByOperator(OperatorEnum $operatorEnum): RulesEvaluatorInterface
     {
             return match($operatorEnum) {
-                OperatorEnum::OR
+                OperatorEnum::OR => new EvaluateConditionsOr(),
+                OperatorEnum::AND => new EvaluateConditionsAnd(),
+                OperatorEnum::NOT=> new EvaluateConditionsNot()
             };
     }
 
