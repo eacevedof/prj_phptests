aa08adb4189f565d027ca905b028126195de2bf0
Eduardo A.F. <eacevedof@yahoo.es>
2024-03-19 20:50:14 +0100

    shell server

---
+++ vendor/Misc/Shell/ShellClient.php
@@ -0,0 +1,46 @@
+<?php
+namespace Misc\Shell;
+
+final class ShellClient
+{
+    private array $commands = [];
+
+    public static function getInstance(): self
+    {
+        return new self();
+    }
+
+    public function addCmd(string $cmd): self
+    {
+        $this->commands[] = $cmd;
+        return $this;
+    }
+    
+    public function exec(): array
+    {
+        if (!$this->commands)
+            return [
+                "output" => [],
+                "result_code" => null,
+            ];
+
+        $output = [];
+        $resultCode = 0;
+
+        $cmds = implode(" ", $this->commands);
+        $cmds = trim($cmds);
+
+        exec($cmds, $output, $resultCode);
+
+        return [
+            "output" => $output,
+            "result_code" => $resultCode,
+        ];
+    }
+
+    public function reset(): self
+    {
+        $this->commands = [];
+        return $this;
+    }
+}
