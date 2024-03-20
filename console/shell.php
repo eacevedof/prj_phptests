4840570875059f5f20dacbac4582f9740b62dc84
Eduardo A.F. <eacevedof@yahoo.es>
2024-03-20 22:03:09 +0100

    console shell

--- console/shell.php
+++ console/shell.php
@@ -1,1 +1,40 @@
 <?php
+
+if ($argc < 2) {
+    echo "Usage: php script.php <param1> <param2> ...\n";
+    exit(1);
+}
+
+include_once "vendor/Misc/Shell/ShellRequest.php";
+include_once "vendor/Misc/Shell/ShellResponse.php";
+include_once "vendor/Misc/Shell/ShellExec.php";
+
+$config = include "vendor/Misc/Shell/shell-client.php";
+
+use Misc\Shell\ShellRequest;
+use Misc\Shell\ShellResponse;
+
+const KEY_ENV = "dev-normon";
+$config = $config[KEY_ENV];
+
+$request = ShellRequest::getInstance();
+$response = ShellResponse::getInstance();
+
+$bearerToken = $response->getTokenFromCache(KEY_ENV);
+if (!$bearerToken) {
+    $output = $request->getAuthToken($config["auth"]);
+    $bearerToken = $response->getTokenFromOutput($output);
+    $response->saveTokenInCache($bearerToken, KEY_ENV);
+}
+
+if (!$bearerToken = $response->getTokenFromCache(KEY_ENV))
+    exit("no token");
+
+$output = $request->postCommand([
+    "url" => $config["shell"]["url"],
+    "bearerToken" => $bearerToken,
+    "sectoken" => $config["shell"]["sectoken"],
+    "command" => "ls -lat",
+]);
+
+$response->printOutput($output);
