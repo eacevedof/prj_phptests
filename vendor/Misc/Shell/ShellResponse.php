1f2fc1cb1397906adc7149e5185d30c48e639258
Eduardo A.F. <eacevedof@yahoo.es>
2024-03-20 20:32:14 +0100

    shell-response

--- vendor/Misc/Shell/ShellResponse.php
+++ vendor/Misc/Shell/ShellResponse.php
@@ -8,10 +8,20 @@ final class ShellResponse
         return new self();
     }
 
-    public function getToken(array $output): string
+    public function getTokenFromOutput(array $output): string
     {
         if (!$output) return "";
         $response = json_decode($output[0], true);
         return $response["data"]["accessToken"] ?? "";
+    }
+
+    public function saveToken(string $bearerToken, string $fileName): void
+    {
+        file_put_contents("./cache/$fileName", $bearerToken);
+    }
+
+    public function getTooken(string $fileName): string
+    {
+        return file_get_contents("./cache/$fileName");
     }
 }
