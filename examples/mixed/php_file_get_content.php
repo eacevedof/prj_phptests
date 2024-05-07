c167187bcb7f0b8d490a5c12ae57ba6853999a33
Eduardo A.F. <eacevedof@yahoo.es>
2024-05-07 12:16:41 +0200

    get-content

--- examples/mixed/php_file_get_content.php
+++ examples/mixed/php_file_get_content.php
@@ -5,7 +5,19 @@
  */
 
 include "vendor/Misc/FileContent/FromBlob/GetFileFromBlob.php";
+include "vendor/Misc/Shell/ShellExec.php";
 
+use Misc\Shell\ShellExec;
 use Misc\FileContent\FromBlob\GetFileFromBlob;
 
-$r = GetFileFromBlob::getInstance()->__invoke();
+$urlRedir = "https://drive.google.com/file/d/1lP8sQ6I-r0C9W0Vn53RWtxopIj9OQwDo/view?usp=drive_link";
+$shell = ShellExec::getInstance();
+$shell->addCommand("curl -i $urlRedir");
+$shell->exec();
+//$shelExec->printDebugCommand();
+bug($shell->getOutput());
+//die;
+$blob = GetFileFromBlob::getInstance();
+$r = $blob->__invoke();
+$blob->withRedirect();
+
--- vendor/Misc/FileContent/FromBlob/GetFileFromBlob.php
+++ vendor/Misc/FileContent/FromBlob/GetFileFromBlob.php
@@ -10,9 +10,37 @@ final class GetFileFromBlob
 
     public function __invoke()
     {
-        $url = "https://laciahubmassfileupload.blob.core.windows.net/miarco/26672%20barcode.gif?sp=racwdl&st=2024-04-30T07:00:00Z&se=2024-05-30T07:00:00Z&spr=https&sv=2022-11-02&sr=c&sig=4XihdXy6bdLL8Uz%2Fubh9%2BP49oi828tFW6Q8123HXVaQ%3D";
+        $urlOk = "https://resources.theframework.es/eduardoaf.com/20200906/095050-logo-eduardoafcom_500.png";
+        $urlBlob = "https://laciahubmassfileupload.blob.core.windows.net/miarco/26672%20barcode.gif?sp=racwdl&st=2024-04-30T07:00:00Z&se=2024-05-30T07:00:00Z&spr=https&sv=2022-11-02&sr=c&sig=4XihdXy6bdLL8Uz%2Fubh9%2BP49oi828tFW6Q8123HXVaQ%3D";
         
-        $content = file_get_contents($url);
+        $content = file_get_contents($urlBlob);
+        bug($content,"content");
         file_put_contents("./upload/some-file.gif", $content);
+
+        bug("done: $urlBlob");
+    }
+
+    public function withRedirect()
+    {
+        $url = "https://drive.google.com/file/d/1lP8sQ6I-r0C9W0Vn53RWtxopIj9OQwDo/view?usp=drive_link";
+
+        $context = stream_context_create([
+            "http" => [
+                "follow_location" => false // Disable automatic redirection
+            ]
+        ]);
+
+        $content = file_get_contents($url, false, $context);
+        $metaData = stream_get_meta_data($context);
+
+        $finalUrl = null;
+        foreach ($metaData["wrapper_data"] as $header) {
+            if (strpos($header, "Location: ") === 0) {
+                $finalUrl = trim(substr($header, strlen("Location: ")));
+                break;
+            }
+        }
+        bug("final url: $finalUrl");
+        //echo "Final URL: $finalUrl";
     }
 }
