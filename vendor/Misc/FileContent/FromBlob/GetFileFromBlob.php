645fce1a63e491e125e9b08c4ffea4461c62fadd
Eduardo A.F. <eacevedof@yahoo.es>
2024-05-07 12:41:42 +0200

    blob test

--- vendor/Misc/FileContent/FromBlob/GetFileFromBlob.php
+++ vendor/Misc/FileContent/FromBlob/GetFileFromBlob.php
@@ -8,11 +8,21 @@ final class GetFileFromBlob
         return new self();
     }
 
-    public function __invoke()
+    public function __invoke(): void
     {
-        $urlOk = "https://resources.theframework.es/eduardoaf.com/20200906/095050-logo-eduardoafcom_500.png";
-        $urlBlob = "https://laciahubmassfileupload.blob.core.windows.net/miarco/26672%20barcode.gif?sp=racwdl&st=2024-04-30T07:00:00Z&se=2024-05-30T07:00:00Z&spr=https&sv=2022-11-02&sr=c&sig=4XihdXy6bdLL8Uz%2Fubh9%2BP49oi828tFW6Q8123HXVaQ%3D";
+        $urls = [
+            "https://resources.theframework.es/eduardoaf.com/20200906/095050-logo-eduardoafcom_500.png",
+            "https://laciahubmassfileupload.blob.core.windows.net/miarco/26672%20barcode.gif?sp=racwdl&st=2024-04-30T07:00:00Z&se=2024-05-30T07:00:00Z&spr=https&sv=2022-11-02&sr=c&sig=4XihdXy6bdLL8Uz%2Fubh9%2BP49oi828tFW6Q8123HXVaQ%3D",
+            
+        ];
+        foreach ($urls as $url) {
+            $this->saveFileFromUrl($url);
+        }
 
+    }
+
+    private function saveFileFromUrl(string $urlBlob): void
+    {
         $fileName = pathinfo($urlBlob, PATHINFO_FILENAME);
         bug($fileName);
         $ext = pathinfo($urlBlob, PATHINFO_EXTENSION);
@@ -21,11 +31,10 @@ final class GetFileFromBlob
         $content = file_get_contents($urlBlob);
         bug($content,"content");
         file_put_contents("./upload/{$fileName}.{$ext[0]}", $content);
-
         bug("done: $urlBlob");
     }
 
-    public function withRedirect()
+    public function withRedirect(): void
     {
         
         $opts = [
