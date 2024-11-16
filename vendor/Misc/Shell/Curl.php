4072cc792aea7f5b7fefd426a5289b43e3f7ffa1
Eduardo A.F. <eacevedof@yahoo.es>
2024-11-16 15:49:24 +0100

    curl

---
+++ vendor/Misc/Shell/Curl.php
@@ -0,0 +1,77 @@
+<?php
+namespace Misc\Shell;
+
+final class Curl
+{
+    private string $logPath = "";
+    private string $location = "";
+    private array $flags = [];
+    private array $headers = [];
+    private array $commands = [];
+    private array $dataRaw = [];
+
+    public static function getInstance(): self
+    {
+        return new self();
+    }
+
+    public function addFlag(string $flag): self
+    {
+        $this->flags[] = $flag;
+        return $this;
+    }
+
+    public function setLocation(string $url): self
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    public function addHeader(string $header,string $value): self
+    {
+        $this->headers[$header] = $value;
+        return $this;
+    }
+
+    private function addDataRaw(string $key, string $value): self
+    {
+        $this->dataRaw[$key] = $value;
+        return $this;
+    }
+
+    private function getCurlCommand(): string
+    {
+        $curl = [
+            "curl",
+        ];
+        foreach ($this->flags as $flag)
+            $curl[] = "-$flag";
+
+        foreach ($this->headers as $header => $value)
+            $curl[] = "--header \"$header: $value\"";
+
+        $jsonPayload = json_encode($this->dataRaw, JSON_UNESCAPED_UNICODE);
+        $curl[] = "--data-raw \'$jsonPayload\'";
+        $curlCmd = implode(" ", $curl);
+        return $curlCmd;
+    }
+
+    private function execAsync(): void
+    {
+        $curlCmd = $this->getCurlCommand();
+        $logPath = $this->logPath;
+        $logPath = "$logPath/curl-async-".date("Ymd").".log";
+        $noHupCmd .= "nohup $curlCmd >> $this->logPath 2>&1 &";
+        echo $noHupCmd;
+        exec($noHupCmd);
+    }
+
+    public function reset(): self
+    {
+        $this->url = "";
+        $this->flags = [];
+        $this->headers = [];
+        $this->dataRaw = [];
+        return $this;
+    }
+}
