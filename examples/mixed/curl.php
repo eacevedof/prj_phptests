abe0684ca5c55f20d33f9ac5213fa5bc79ab36b0
Eduardo A.F. <eacevedof@yahoo.es>
2024-11-16 19:27:52 +0100

    curl cmd

--- examples/mixed/curl.php
+++ examples/mixed/curl.php
@@ -9,7 +9,8 @@ $curl = Curl::getInstance();
 $curl
     ->setLogPath(__DIR__."/../../logs/debug")
     ->addFlag("s")
-    ->setLocation("http://localhost:8080")
+    //->setLocation("http://localhost:8080")
+    ->setLocation("https://json.theframework.es/index.php?getfile=app_costumer.json")
     ->addHeader("Content-Type", "application/json")
     ->addDataRaw("name", "dimail")
     ->execAsync()
